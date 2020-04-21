29fb3be9ea7196dadedad4e12cf0f4e998d04db0
Parshva Timbadia <parshwat@gmail.com>
2020-04-21 16:11:32 +0530

    Create Kth Largest Number.py

---
+++ Medium/Kth Largest Number.py
@@ -0,0 +1,34 @@
+***
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Example 1:
+
+Input: [3,2,1,5,6,4] and k = 2
+Output: 5
+Example 2:
+
+Input: [3,2,3,1,2,4,5,5,6] and k = 4
+Output: 4
+Note:
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+***
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+         
+             
+        min_heap=[]
+        for i in range(k):
+
+            heappush(min_heap, nums[i])
+            
+
+
+
+        for i in range(k, len(nums)):
+            if  nums[i]> min_heap[0]:
+                heappop(min_heap)
+                heappush(min_heap, nums[i])  
+
+        return min_heap[0]    
