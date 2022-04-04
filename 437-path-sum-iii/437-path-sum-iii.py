0adb758907a9e0b5e5048107eb36b87ba22b79d2
Parshva Timbadia <parshwat@gmail.com>
2022-04-03 20:35:16 -0700

    Time: 69 ms (74.45%), Space: 15.2 MB (92.58%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        
+        
+        hMap = {0:1}
+        count = 0
+        def helper(node, sumSoFar, hMap):
+            nonlocal count
+            
+            if node is None:
+                return 
+            
+            sumSoFar += node.val
+            
+            if sumSoFar - targetSum in hMap:
+                count += hMap[sumSoFar - targetSum]
+            
+            if sumSoFar not in hMap:
+                hMap[sumSoFar] = 0
+            
+            hMap[sumSoFar] += 1
+            
+            helper(node.left, sumSoFar, hMap)
+            helper(node.right, sumSoFar, hMap)
+            
+            if sumSoFar in hMap:
+                hMap[sumSoFar] -= 1
+                if hMap[sumSoFar] == 0:
+                    del hMap[sumSoFar]
+        
+        helper(root, 0, hMap)
+        return count
+            
+            
+            
+            
+            
