69bb18a3ad102d82e06774aeb5265d4f75274772
Parshva Timbadia <parshwat@gmail.com>
2022-05-02 18:02:09 -0700

    Time: 76 ms (24.61%), Space: 16.7 MB (29.69%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -4,18 +4,18 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import math
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        
-        def helper(root, minNode, maxNode):
+        def helper(node, leftbound, rightbound):
             
-            if root is None:
+            if node is None:
                 return True
             
-            if not minNode < root.val < maxNode:
+            if not leftbound < node.val < rightbound:
                 return False
             
-            return helper(root.left, minNode, root.val) and helper(root.right, root.val, maxNode)   
+            return helper(node.left, leftbound, node.val) and helper(node.right, node.val, rightbound)
         
         return helper(root, -math.inf, math.inf)
