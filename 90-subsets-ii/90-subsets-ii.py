ac2291a4074c187b6ab425ccd267151731ce080a
Parshva Timbadia <parshwat@gmail.com>
2022-05-08 20:40:17 -0700

    Time: 69 ms (19.58%), Space: 14.2 MB (50.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,10 +1,8 @@
-from typing import List
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         
-        
         nums.sort()
-        result: List[List[int]] = []
+        result = []
         def helper(index, slate):
             
             if index >= len(nums):
@@ -12,23 +10,24 @@ class Solution:
                 return
             
             counts = 1
-            j = index + 1
-            
-            while j < len(nums) and nums[index] == nums[j]:
-                j += 1
-                counts += 1
+            i = index + 1
+            while (i < len(nums) and nums[i] == nums[i-1]):
+                counts+=1
+                i+=1
             
             for count in range(0, counts+1):
                 
                 for _ in range(count):
                     slate.append(nums[index])
                 
-                helper(index+counts, slate)
+                helper(index+counts,slate)
                 
                 for _ in range(count):
                     slate.pop()
         
         helper(0, [])
         return result
+            
+            
+            
         
-                
