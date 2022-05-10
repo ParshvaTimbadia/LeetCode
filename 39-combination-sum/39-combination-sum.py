13cf612c63046c330801a7cfa8f4f9f01b90da6b
Parshva Timbadia <parshwat@gmail.com>
2022-05-09 22:16:34 -0700

    Time: 100 ms (62.32%), Space: 13.9 MB (91.72%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,22 +1,29 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
+        
         result = []
-        def helper(index, sumSoFar, slate):
+        def helper(index,sumSoFar, slate):
             
-            if sumSoFar > target or index >= len(candidates):
-                return 
+            #Backtracking Case
+            if sumSoFar > target or index == len(candidates):
+                return
             
-            if sumSoFar == target:
+            #BaseCondition
+            if target == sumSoFar:
                 result.append(slate[:])
                 return
             
+            #include
             slate.append(candidates[index])
             helper(index, sumSoFar + candidates[index], slate)
             slate.pop()
             
+            
+            #Exclude
             helper(index+1, sumSoFar, slate)
         
         helper(0, 0, [])
         return result
+            
             
