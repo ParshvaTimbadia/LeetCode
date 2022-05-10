a40a39e3b9d2e73c65cb216ac69cb919384cc4f5
Parshva Timbadia <parshwat@gmail.com>
2022-05-09 22:07:48 -0700

    Time: 54 ms (33.30%), Space: 14.2 MB (34.15%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,32 +1,32 @@
-from typing import List
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
-        
-        # Partial Soltion: slate
-        # Subproblem Definition: index
-        result: List[int] = []
-        def helper(index:int, slate: List) -> None:
+        result = []
+        def helper(index,slate):
             
+            #Base Condition:
             if index == len(nums):
                 result.append(slate[:])
                 return
             
-            #Two cases -> Either include or exclude
+            #Exclude
             helper(index+1, slate)
             
+            #Include
+            
+            #Do
             slate.append(nums[index])
             helper(index+1, slate)
+            #Undo
             slate.pop()
         
         helper(0, [])
         return result
-    
+            
         """
-        TimeComplexity: Every position will have 2 possibilities to either include or exclude. O(2^n)
-        Copying the slate to the result will take O(n).
-        Overall : O(n*2^n)
         
         
-        Space Complexity: O(n*2^n)
+        
+        
+        
         """
