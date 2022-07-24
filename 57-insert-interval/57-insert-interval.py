dfb5c5fb88af8638c4e2216c5331dcd344e23bef
Parshva Timbadia <parshwat@gmail.com>
2022-07-23 20:04:45 -0700

    Time: 200 ms (5.16%), Space: 17.3 MB (92.25%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,23 +1,29 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         
+        
         i = 0 
-        result = []
+
+        result = []        
         while i < len(intervals) and intervals[i][1] < newInterval[0]:
             result.append(intervals[i])
             i += 1
         
-        startIntervals = newInterval[0]
-        endIntervals = newInterval[1]
-        while i < len(intervals) and endIntervals >= intervals[i][0]:
-            startIntervals = min(startIntervals, intervals[i][0])
-            endIntervals = max(endIntervals, intervals[i][1])
+        #Time to merge the new Interval.
+        start = newInterval[0]
+        end = newInterval[1]
+        
+        while i < len(intervals) and end >= intervals[i][0]:
+            start = min(start, intervals[i][0])
+            end = max(end, intervals[i][1])
             i += 1
         
-        result.append([startIntervals, endIntervals])
+        result.append([start, end])
         
         while i < len(intervals):
             result.append(intervals[i])
-            i+=1
+            i += 1
         
         return result
+            
+        
