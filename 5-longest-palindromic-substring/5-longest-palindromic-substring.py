74f8ccac4a39b17096d8503fcfc022f9c9383229
Parshva Timbadia <parshwat@gmail.com>
2022-08-07 19:15:24 -0700

    Time: 7272 ms (8.42%), Space: 22.7 MB (7.48%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,38 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        
+    
+        """
+
+        dp(start, end) = dp(start + 1, end - 1) // Should be true
+
+        else: 
+
+        find the the palindrome from dp(start + 1, end) or dp(start, end - 1)
+
+        """
+
+        dp = [[False for i in range(len(s))] for j in range(len(s))]
+        
+        start = -1
+        maxLength = 1
+
+        #Interestingly, every individual letter will be a palindrome.
+
+        for i in range(len(s)):
+            dp[i][i] = True
+
+        for startIndex in range(len(s) - 1, -1, -1):
+            for endIndex in range(startIndex + 1, len(s)):
+
+                if s[startIndex] == s[endIndex]:
+                    if endIndex - startIndex == 1 or dp[startIndex + 1][endIndex - 1]:
+                        dp[startIndex][endIndex] = True
+                        
+                        if endIndex - startIndex + 1 > maxLength:
+                            maxLength = max(maxLength, endIndex - startIndex + 1)
+                            start = startIndex
+                        
+
+        return s[start: start+maxLength] if maxLength > 1 else s[0]
+    
