4bc19f3f883bdb52f2c2888b9077125334875611
Parshva Timbadia <parshwat@gmail.com>
2022-08-09 18:33:38 -0700

    Time: 515 ms (84.60%), Space: 15.5 MB (75.57%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -1,19 +1,23 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
-        end = max(piles)
-        start = 1
         
-        def calculateTotalHours(hours):
+        start = 1
+        end = max(piles)
+        
+        def calc(number_of_bananas):
             result = 0
-            for pile in piles:
-                result += math.ceil(pile/hours)
+            for b in piles:
+                result += math.ceil(b/number_of_bananas)
             
             return result
         
         while start <= end:
-            mid = start + (end - start)//2
-            if calculateTotalHours(mid) <= h:
+            
+            mid = start + (end-start)//2
+            
+            if calc(mid) <= h:
+                #Mean koko can eat slow
                 end = mid - 1
             else:
                 start = mid + 1
