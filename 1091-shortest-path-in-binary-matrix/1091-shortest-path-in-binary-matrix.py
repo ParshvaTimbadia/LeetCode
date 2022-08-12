b8859623c8c49253ae29b58561cb0d217ed979b8
Parshva Timbadia <parshwat@gmail.com>
2022-08-12 03:47:40 -0700

    Time: 1116 ms (35.80%), Space: 15.4 MB (38.78%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,38 +1,42 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         
-        if grid[0][0] == 1 or grid[len(grid)-1][len(grid[0]) - 1] == 1:
+        if grid[0][0] != 0 or grid[-1][-1] != 0:
             return -1
-    
-        visited = set()
+        
+        n = len(grid)
         queue = deque()
-        queue.append((0,0,1))
-        visited.add((0,0))
+        queue.append((0,0))
+        visited = set((0,0))
         
         
-        neighbours = [(1, 0), (0,1), (-1, 0), (0, -1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+        neighbors = [(0,1), (1,0), (-1, 0), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+        distance = 1
         
         while queue:
             
-            row, col, distance = queue.popleft()
-            
-            if row == len(grid) - 1 and col == len(grid[0]) - 1:
-                return distance
-            
-            for r, c in neighbours:
+            for _ in range(len(queue)):
                 
-                if not( 0<= row + r < len(grid) and 0 <= col + c < len(grid[0])) or (row+r,col+c) in visited or grid[row+r][col+c] == 1:
-                    continue
+                row, col = queue.popleft()
                 
-                queue.append((row+r, col+c, distance + 1))
-                visited.add((row+r, col+c))
+                
+                if row == n - 1 and col == n - 1:
+                    return distance
+                
+                for r, c in neighbors:
+                    
+                    if not(0 <= row + r < n and 0 <= col + c < n) or (row + r, col + c) in visited or grid[row + r][col + c]==1:
+                        continue
+                    
+                    visited.add((row + r, col + c))
+                    queue.append((row + r, col + c))
+                    
+        
+            distance += 1
+        
         
         return -1
-                
-                
             
             
-                
-                
-                
-                
+            
+            
