9f3436311b4cdf0045da66406f81975aa567ae49
Parshva Timbadia <parshwat@gmail.com>
2022-08-12 16:48:39 -0700

    Time: 53 ms (97.98%), Space: 14.4 MB (57.47%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -1,34 +1,42 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-    
+        
+        
         def find(x):
-            if parent[x] == x:
+            
+            if x == parent[x]:
                 return x
             
-            rootx = find(parent[x])
-            parent[x] = rootx
+            root = find(parent[x])
+            parent[x] = root
             
-            return rootx
+            return parent[x]
         
-        
-        
-        parent = [i for i in range(0, len(edges)+1)]
-        size = [1]*(len(edges)+1)
-        result = []
-        
-        for u, v in edges:
-            findu = find(u)
-            findv = find(v)
+        n = set()
+        for edge in edges:
+            u = edge[0]
+            v = edge[1]
             
-            if findu == findv:
-                result.append([u, v])
+            n.add(u)
+            n.add(v)
+        
+        parent = [i for i in range(len(n)+1)]
+        size = [1 for i in range(len(n)+1)]
+
+        
+        for edge in edges:
+            rootU = find(edge[0])
+            rootV = find(edge[1])
+            
+            if rootU == rootV:
+                result = [edge[0], edge[1]]
             else:
-                if size[findu] < size[findv]:
-                    parent[findu] = findv
-                    size[findv] += size[findu]
+                if size[rootU] < size[rootV]:
+                    size[rootV] += size[rootU]
+                    parent[rootU] = rootV
                 else:
-                    parent[findv] = findu
-                    size[findu] += size[findv]
+                    size[rootU] += size[rootV]
+                    parent[rootV] = rootU
         
-        return result[-1]
-                    
+        return result
+        
