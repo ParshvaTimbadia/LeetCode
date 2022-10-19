f055dcdb573959f1a31c49faf55c0f7ce0555fb7
Parshva Timbadia <parshwat@gmail.com>
2022-10-19 14:25:19 -0700

    Time: 214 ms (37.39%), Space: 14.8 MB (38.61%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,23 +1,36 @@
+class Transaction(object):
+    def __init__(self, txn):
+        name, time, amount, city = txn.split(',')
+        self.name = name
+        self.time = int(time)
+        self.amount = int(amount)
+        self.city = city
+        
+    def __str__(self):
+        return "{},{},{},{}".format(self.name, self.time, self.amount, self.city)
+
+
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         
+        for i in range(len(transactions)):
+            transactions[i] = Transaction(transactions[i])
+            
+        
         result = []
         for i in range(len(transactions)):
-            name, time, amount, city = transactions[i].split(",")
-            if int(amount) > 1000:
+            if int(transactions[i].amount) > 1000:
                 result.append(transactions[i])
                 continue
             
             for j, t in enumerate(transactions):
                 if j == i:
-                    continue
-                sname, stime, samount, scity = transactions[j].split(",")
-                
-                if sname == name and scity != city and abs(int(stime) - int(time)) <= 60:
+                    continue                
+                if transactions[i].name == transactions[j].name and transactions[i].city != transactions[j].city and abs(transactions[i].time - transactions[j].time) <= 60:
                     result.append(transactions[i])
                     break
         
-        return result
+        return [r.__str__() for r in result]
                 
                 
                 
