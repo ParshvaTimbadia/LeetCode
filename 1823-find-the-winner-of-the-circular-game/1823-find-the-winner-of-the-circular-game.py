930e0d23d66971a186014a7cdaa479d23ac7b21a
Parshva Timbadia <parshwat@gmail.com>
2022-10-20 07:44:28 -0700

    Time: 71 ms (56.62%), Space: 14.2 MB (13.75%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,22 +1,15 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
-        queue = deque()
-        for i in range(1, n+1):
-            queue.append(i)
+        def helper(n, k):
+            if n == 1:
+                return 0
+            x = helper(n-1, k)
+            y = (x + k) % n
+            return y
         
-        while len(queue)!=1:
-            
-            #Remove first K-1 elements from the queue.
-            i = 0 
-            while i != k -1:
-                queue.append(queue.popleft())
-                i += 1
-            
-            #Remove kth element
-            queue.popleft()
+        return helper(n, k) + 1
         
-        return queue[0]
     
         # TimeComplexity: O(N**2)
         # Space Complexity: Since we are using the queue of size N, it would be O(N).
