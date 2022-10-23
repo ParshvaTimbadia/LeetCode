49e462c7d661ddf3cf938b211973f95edc9360e0
Parshva Timbadia <parshwat@gmail.com>
2022-10-22 20:28:21 -0700

    Time: 1425 ms (56.44%), Space: 14 MB (57.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,35 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+    
+        #TC- O(N2)
+        #SC - O(1)
         
-        dp = [[False for i in range(len(s))] for j in range(len(s))]
         
-        start = -1
-        maxLength = 1
-
-        #Interestingly, every individual letter will be a palindrome.
-
+        # aba
+        #  ^
+        # abba
+        #  ^^
+        
+        def expandAroundCenter(start, end):
+            while 0 <= start and end < len(s) and s[start] == s[end]:
+                start -= 1
+                end += 1
+            start += 1
+            end -= 1
+            return end - start + 1, start, end
+        
+        maxLength = 0
+        result = ""
         for i in range(len(s)):
-            dp[i][i] = True
-
-        for startIndex in range(len(s) - 1, -1, -1):
-            for endIndex in range(startIndex + 1, len(s)):
-
-                if s[startIndex] == s[endIndex]:
-                    if endIndex - startIndex == 1 or dp[startIndex + 1][endIndex - 1]:
-                        dp[startIndex][endIndex] = True
-                        
-                        if endIndex - startIndex + 1 > maxLength:
-                            maxLength = max(maxLength, endIndex - startIndex + 1)
-                            start = startIndex
-                        
-
-        return s[start: start+maxLength] if maxLength > 1 else s[0]
+            len1 = expandAroundCenter(i, i)
+            len2 = expandAroundCenter(i, i+1)
+            length, start, end = max(len1, len2)
+            if maxLength < length:
+                result = s[start:end+1]
+                maxLength = max(maxLength, length)
+        
+        return result
+        
+            
+            
+            
